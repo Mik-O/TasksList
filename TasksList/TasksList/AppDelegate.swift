00fd5c5d0251b7c16e34f596c7ff442232ab7166
mikle <mikle.kontinent@gmail.com>
2025-07-18 14:58:34 +0700

    work with New Task View Controller

--- TasksList/TasksList/AppDelegate.swift
+++ TasksList/TasksList/AppDelegate.swift
@@ -19,5 +19,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = UINavigationController(rootViewController: TaskListViewController())
         return true
     }
+    
+    // MARK: - Core Data stack (в домашнем задании вынести это в storge manager)
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "TasksList")
+        container.loadPersistentStores(completionHandler: { storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support (в домашнем задании вынести это в storge manager)
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
 }
 
--- TasksList/TasksList/TaskListViewController.swift
+++ TasksList/TasksList/TaskListViewController.swift
@@ -6,13 +6,25 @@
 //
 
 import UIKit
+import CoreData
 
 class TaskListViewController: UITableViewController {
+    
+    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    private let cellID = "cellID"
+    private var taskList: [Task] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
         setupNavigationBar()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchData()
     }
 
     private func setupNavigationBar() {
@@ -49,6 +61,34 @@ class TaskListViewController: UITableViewController {
     
     @objc private func addNewTask() {
         let newTaskVC = NewTaskViewController()
+        newTaskVC.modalPresentationStyle = .fullScreen
         present(newTaskVC, animated: true)
+    }
+    
+    private func fetchData() {
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        
+        do {
+            taskList = try context.fetch(fetchRequest)
+            tableView.reloadData()
+        } catch let error {
+            print(error)
+        }
+    }
+}
+
+    // MARK: - Table View Data Sourse
+extension TaskListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        taskList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        let task = taskList[indexPath.row]
+        content.text = task.name
+        cell.contentConfiguration = content
+        return cell
     }
 }
